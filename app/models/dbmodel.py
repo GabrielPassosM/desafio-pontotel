c5cda437fb2669f659c7f17d8dd44dd716def40b
GabrielPassosM <gabrielpassosmartins@gmail.com>
2021-02-11 12:06:49 -0300

    update db url

--- app/models/dbmodel.py
+++ app/models/dbmodel.py
@@ -2,7 +2,7 @@ from app import app
 from flask_sqlalchemy import SQLAlchemy
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:2gabriel1@localhost/alphaempresas'
+app.config['DATABASE_URL'] = 'postgres://ccecbcuhcurema:94271a951c166a9408789f18cbdfc8280cc6aa4a2c48020a483d6c94b3da11a2@ec2-3-211-245-154.compute-1.amazonaws.com:5432/d6eejfo8l1uscq'
 db = SQLAlchemy(app)
 
 class Empresa(db.Model):
